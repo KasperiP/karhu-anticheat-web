055bc0b3bc68c2b8b507ecb37dbecbdac824a3f5
kassq <kasperi.pohtine@gmail.com>
2022-08-27 11:58:10 +0300

    Revert back to older stripe version

--- src/pages/api/webhooks/index.ts
+++ src/pages/api/webhooks/index.ts
@@ -10,7 +10,7 @@ import Customers from '../../../models/customerModel'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
     // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2022-08-01',
+    apiVersion: '2020-08-27',
 })
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!
